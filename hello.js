4c207dec23239e897bdfbd7a47f04f41c1cb5bf3
manya <manyaagureja0@gmail.com>
2024-09-06 14:14:14 +0530

    my first commit

---
+++ hello.js
@@ -0,0 +1,14 @@
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Variables
+let num1 = 5;
+let num2 = 10;
+
+// Call the function and store the result
+let result = addNumbers(num1, num2);
+
+// Display the result in the console
+console.log("The sum is: " + result);
---
+++ index.js
@@ -0,0 +1,19 @@
+const express= require('express')
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    return res.send('hello from home page')
+})
+
+app.get('/about',(req,res)=>{
+    return res.send('hello from about page '+'hey '+req.query.name)
+})
+
+
+function myhandler(req,res){
+
+}
+const server=http.createServer(app)
+
+server.listen(3000)
